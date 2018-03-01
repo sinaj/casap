693c68a4a9b729370fa89bf064acf110fda5b7ac
olivaC <coliva@ualberta.ca>
2018-03-01 13:08:52 -0700

    Remove autoscroll

--- static/js/public/index.js
+++ static/js/public/index.js
@@ -9,16 +9,14 @@ $(document).ready(function () {
         slidesToScroll: 4,
         arrows: true,
         dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
+        autoplay: false,
         responsive: [
             {
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
-                    autoplay: true,
-                    autoplaySpeed: 5000,
+                    autoplay: false,
                     infinite: true,
                     dots: true
                 }
