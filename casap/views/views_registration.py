7630c845829f7e533b8bca1d57f063cf15a92877
olivaC <coliva@ualberta.ca>
2018-03-12 13:32:14 -0600

    #102 Move redirect to outside for loop to save volunteer availabilities

--- casap/views/views_registration.py
+++ casap/views/views_registration.py
@@ -105,7 +105,7 @@ def register_volunteer_view(request):
                                                                  km_radius=f.cleaned_data['km_radius'])
                             availability.save()
                             add_message(request, messages.SUCCESS, "Volunteer Registration was successful.")
-                            return HttpResponseRedirect(request.POST.get("next", reverse("index")))
+                return HttpResponseRedirect(request.POST.get("next", reverse("index")))
 
             else:
                 for f in formset:
